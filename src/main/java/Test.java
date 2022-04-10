1750d9219e2e95802b5fbcc75b0d349d18dec88a
zyan <ww9720@gmail.com>
2022-04-11 07:32:23 +0900

    second commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+import io.vertx.core.Vertx;
+
+public class Test {
+    public static void main(String[] args) {
+        Vertx v = Vertx.vertx();
+        v.deployVerticle
+        System.out.println("success");
+    }
+}
